d449bac0ed317f05dacb0e3edc49e8b0883b45e1
Vishnu Sharma <162160016+svishnu1601@users.noreply.github.com>
2025-07-12 23:18:10 +0530

    Add files via upload

--- IntegerIndexedElementGet.js
+++ IntegerIndexedElementGet.js
@@ -4,19 +4,18 @@ var $TypeError = require('es-errors/type');
 
 var GetValueFromBuffer = require('./GetValueFromBuffer');
 var IsValidIntegerIndex = require('./IsValidIntegerIndex');
+var TypedArrayElementSize = require('./TypedArrayElementSize');
+var TypedArrayElementType = require('./TypedArrayElementType');
 
+var isTypedArray = require('is-typed-array');
 var typedArrayBuffer = require('typed-array-buffer');
 var typedArrayByteOffset = require('typed-array-byte-offset');
-var whichTypedArray = require('which-typed-array');
 
-var tableTAO = require('./tables/typed-array-objects');
-
-// https://262.ecma-international.org/12.0/#sec-integerindexedelementget
+// https://262.ecma-international.org/13.0/#sec-integerindexedelementget
 
 module.exports = function IntegerIndexedElementGet(O, index) {
-	var arrayTypeName = whichTypedArray(O); // step 4
-	if (!arrayTypeName) {
-		throw new $TypeError('Assertion failed: `O` must be a TypedArray'); // step 1
+	if (!isTypedArray(O)) {
+		throw new $TypeError('Assertion failed: `O` must be a TypedArray');
 	}
 
 	if (typeof index !== 'number') {
@@ -24,16 +23,16 @@ module.exports = function IntegerIndexedElementGet(O, index) {
 	}
 
 	if (!IsValidIntegerIndex(O, index)) {
-		return void undefined; // step 2
+		return void undefined; // step 1
 	}
 
-	var offset = typedArrayByteOffset(O); // step 3
+	var offset = typedArrayByteOffset(O); // step 2
 
-	var elementType = tableTAO.name['$' + arrayTypeName]; // step 7
+	var elementSize = TypedArrayElementSize(O); // step 3
 
-	var elementSize = tableTAO.size['$' + elementType]; // step 5
+	var indexedPosition = (index * elementSize) + offset; // step 4
 
-	var indexedPosition = (index * elementSize) + offset; // step 6
+	var elementType = TypedArrayElementType(O); // step 5
 
 	return GetValueFromBuffer(typedArrayBuffer(O), indexedPosition, elementType, true, 'Unordered'); // step 11
 };
--- IntegerIndexedElementSet.js
+++ IntegerIndexedElementSet.js
@@ -6,39 +6,37 @@ var IsValidIntegerIndex = require('./IsValidIntegerIndex');
 var SetValueInBuffer = require('./SetValueInBuffer');
 var ToBigInt = require('./ToBigInt');
 var ToNumber = require('./ToNumber');
+var TypedArrayElementSize = require('./TypedArrayElementSize');
+var TypedArrayElementType = require('./TypedArrayElementType');
 
 var typedArrayBuffer = require('typed-array-buffer');
 var typedArrayByteOffset = require('typed-array-byte-offset');
 var whichTypedArray = require('which-typed-array');
 
-var tableTAO = require('./tables/typed-array-objects');
-
-// https://262.ecma-international.org/12.0/#sec-integerindexedelementset
+// https://262.ecma-international.org/13.0/#sec-integerindexedelementset
 
 module.exports = function IntegerIndexedElementSet(O, index, value) {
+	var arrayTypeName = whichTypedArray(O);
+	if (!arrayTypeName) {
+		throw new $TypeError('Assertion failed: `O` must be a TypedArray');
+	}
+
 	if (typeof index !== 'number') {
 		throw new $TypeError('Assertion failed: `index` must be a Number');
 	}
 
-	var arrayTypeName = whichTypedArray(O); // step 4.b
-	if (!arrayTypeName) {
-		throw new $TypeError('Assertion failed: `O` must be a TypedArray'); // step 1
-	}
-
 	var contentType = arrayTypeName === 'BigInt64Array' || arrayTypeName === 'BigUint64Array' ? 'BigInt' : 'Number';
-	var numValue = contentType === 'BigInt' ? ToBigInt(value) : ToNumber(value); // steps 2 - 3
+	var numValue = contentType === 'BigInt' ? ToBigInt(value) : ToNumber(value); // steps 1 - 2
 
-	if (IsValidIntegerIndex(O, index)) { // step 4
-		var offset = typedArrayByteOffset(O); // step 4.a
+	if (IsValidIntegerIndex(O, index)) { // step 3
+		var offset = typedArrayByteOffset(O); // step 3.a
 
-		var elementType = tableTAO.name['$' + arrayTypeName]; // step 4.e
+		var elementSize = TypedArrayElementSize(O); // step 3.b
 
-		var elementSize = tableTAO.size['$' + elementType]; // step 4.c
+		var indexedPosition = (index * elementSize) + offset; // step 3.c
 
-		var indexedPosition = (index * elementSize) + offset; // step 4.d
+		var elementType = TypedArrayElementType(O); // step 3.d
 
-		SetValueInBuffer(typedArrayBuffer(O), indexedPosition, elementType, numValue, true, 'Unordered'); // step 4.e
+		SetValueInBuffer(typedArrayBuffer(O), indexedPosition, elementType, numValue, true, 'Unordered'); // step 3.e
 	}
-
-	// 5. Return NormalCompletion(undefined)
 };
